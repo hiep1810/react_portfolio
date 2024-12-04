5de295609bb4596b75f140ef39f91b1ece59276e
HiepTT <hiep981018@gmail.com>
2024-12-04 22:25:34 +0700

    feat: add Title in Menu Bar

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -6,6 +6,7 @@ export default function MenuBar() {
   return (
     <div className="bg-[#252526] text-[#CCCCCC] text-sm border-b border-[#3C3C3C]">
       <div className="flex items-center">
+        {/* Menu items */}
         <div className="flex-1 flex">
           {menuItems.map((item) => (
             <button
@@ -16,6 +17,11 @@ export default function MenuBar() {
             </button>
           ))}
         </div>
+        {/* Title */}
+        <div className="flex-1 flex items-center">
+            <span>Tuan Hiep Truong - Portfolio</span>
+        </div>
+        {/* Status bar */}
         <div className="flex items-center pr-2">
           <div className="w-3 h-3 rounded-full bg-[#FF6057] mx-1"></div>
           <div className="w-3 h-3 rounded-full bg-[#FFBD2E] mx-1"></div>
