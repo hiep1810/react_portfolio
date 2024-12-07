3ec318f009207fd1ee687dd6b7f9c4b43accc5dc
hiep1810 <33901647+hiep1810@users.noreply.github.com>
2024-12-07 20:21:11 +0700

    fix: add IconButtonProvider for projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,10 +1,13 @@
 import { TabProvider } from "@/context/TabContext";
 import ProjectsScreen from "@/components/ProjectsScreen";
+import { IconButtonProvider } from "@/context/IconButtonContext";
 
 export default function ProjectsPage() {
   return (
+    <IconButtonProvider>
     <TabProvider>
-      <ProjectsScreen />
-    </TabProvider>
+        <ProjectsScreen />
+      </TabProvider>
+    </IconButtonProvider>
   );
 } 
