745032fb24793d60b8cf9580a927d56c2212cad4
hiep1810 <33901647+hiep1810@users.noreply.github.com>
2024-12-07 22:35:09 +0700

    fix: fix cant close drawer bug

--- src/components/layout/Drawer.tsx
+++ src/components/layout/Drawer.tsx
@@ -14,10 +14,9 @@ export default function Drawer() {
   const [isOpen, setIsOpen] = useState(true)
 
   return (
-    <div className={`bg-[#252526] text-[#CCCCCC] w-64 flex-shrink-0 overflow-y-auto border-r border-[#3C3C3C] 
+    <div className={`bg-[#252526] text-[#CCCCCC] flex-shrink-0 overflow-y-auto border-r border-[#3C3C3C] 
       ${!activeIconButton ? 'w-0' : 'w-64'} transition-[width] duration-300`}>
-      <div className={`flex items-center justify-between mb-2 ${!activeIconButton ? 'opacity-0' : 'opacity-100'} 
-        transition-opacity duration-300`}>
+      <div className={`flex items-center justify-between mb-2`}>
         <span className="p-2">EXPLORER</span>
         <MoreHorizontal size={16} className="mr-2" />
       </div>
