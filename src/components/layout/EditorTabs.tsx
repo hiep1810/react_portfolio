f6273349a445bf2876f28a4b02ac91e047c4af84
hiep1810 <33901647+hiep1810@users.noreply.github.com>
2024-12-08 20:44:00 +0700

    refactor: remove unused useEffect

--- src/components/layout/EditorTabs.tsx
+++ src/components/layout/EditorTabs.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { X } from 'lucide-react'
 import Image from 'next/image'
 import { initialTabs } from '@/data/tabs'
